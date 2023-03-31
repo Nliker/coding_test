44ca93fd73470482a68d195069ed75e839b56588
unknown <tjwjdgus83@naver.com>
2023-03-31 16:08:30 +0900

    파일합치기

--- short_growth.py
+++ short_growth.py
@@ -1,7 +1,7 @@
 from collections import deque
 import sys
 import copy
-from heapq import heappop,heappush
+from heapq import heappop,heappush,heapify
 import math
 
 def bj_12865():
@@ -130,3 +130,61 @@ def bj_10830():
 # result=bj_10830()
 # for array in result:
 #     print(" ".join(map(str,array)))
+
+def array_mul_pibo(a,b):
+    result=[[0 for _ in range(len(b[0]))] for _ in range(len(a))]
+    for i in range(len(a)):
+        for j in range(len(b[0])):
+            sum=0
+            for k in range(len(b)):
+                sum=sum+a[i][k]*b[k][j]
+            result[i][j]=sum%1000000
+    return result
+
+def power_pibo(array,B):
+    if B==1:
+        return array
+    array_B=power_pibo(array,B//2)
+    if B%2==0:
+        return array_mul_pibo(array_B,array_B)
+    else:
+        return array_mul_pibo(array_mul_pibo(array_B,array_B),array)
+
+def bj_2749():
+    n=int(input())
+    init_array=[[1,1],[1,0]]
+    factor_array=power_pibo(init_array,n)
+    return array_mul_pibo(factor_array,[[1],[0]])[1][0]
+# print(bj_2749())
+# print(array_mul_pibo([[1,2],[3,4]],[[1,2],[3,4]]))
+
+def bj_11066():
+    t=int(sys.stdin.readline())
+    result=[]
+    for i in range(t):
+        n=int(sys.stdin.readline())
+        num_list=[0]+list(map(int,sys.stdin.readline().rstrip().split()))
+        dp_list=[[0 for _ in range(n+1)] for _ in range(n+1)]
+        
+        for i in range(n,0,-1):
+            for j in range(i+1,n+1):
+                page_list=[dp_list[i][k]+dp_list[k+1][j] for k in range(i,j)]
+                dp_list[i][j]=min(page_list)+sum(num_list[i:j+1])
+
+        result.append(dp_list[1][n])
+    return result
+
+result=bj_11066()
+for min_cost in result:
+    print(min_cost)
+# f(1,2)->f(1,1)+f(2,2)
+# f(1,3)->f(1,1)+f(2,3)
+#         f(1,2)+f(3,3)
+# f(1,4)->f(1,1)+f(2,4)
+#         f(1,2)+f(3,4)
+#         f(1,3)+f(4,4)
+#     A B C D
+# A   
+# B
+# C
+# D
